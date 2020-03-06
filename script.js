ae62913b2f62ef61368a2889722e678954042226
Fhernandez248 <61877456+Fhernandez248@users.noreply.github.com>
2020-03-06 09:42:05 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,111 @@
+// DECLARE YOUR VARIABLES HERE
+
+
+
+
+
+
+
+
+let teamOneName;
+teamOneName="East";
+let teamTwoName;
+teamTwoName="West";
+
+let scoreOne;
+scoreOne=0;
+let scoreTwo;
+scoreTwo=0;
+
+let freethrow;
+freethrow=1;
+let twoPointShot;
+twoPointShot=2;
+let threePointShot;
+threePointShot=3;
+let arena="Staples Center";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DON'T CHANGE THIS CODE
+updateTeams();
+
+let realTeamOneName, realTeamTwoName, realScoreOne, realScoreTwo, realThreePointShot, realTwoPointShot, realFreeThrow;
+
+try {
+    realTeamOneName = eval('teamOneName');
+    realTeamTwoName = eval('teamTwoName');
+    realScoreOne = eval('scoreOne');
+    realScoreTwo = eval('scoreTwo');
+    realThreePointShot = eval('threePointShot');
+    realTwoPointShot = eval('twoPointShot');
+    realFreeThrow = eval('freeThrow');
+} catch (_e) {}
+
+updateTeams();
+
+$("button").click(function(){
+    var team;
+    team = $(this).attr("team");
+    
+    var points;
+    points = $(this).attr("class");
+    
+    updateScore(team, points);
+});
+
+function updateScore(myTeam, myPoints){
+    if(myTeam === "one"){
+        if(myPoints === "twoPt"){
+            realScoreOne += realTwoPointShot;
+        } else if(myPoints === "threePt"){
+            realScoreOne += realThreePointShot;
+        } else if(myPoints === "freeThrow"){
+            realScoreOne += realFreeThrow;
+        }
+        $(".scoreOne").text(realScoreOne);
+    } else if (myTeam === "two"){
+        if(myPoints === "twoPt"){
+            realScoreTwo += realTwoPointShot;
+        } else if(myPoints === "threePt"){
+            realScoreTwo += realThreePointShot;
+        } else if(myPoints === "freeThrow"){
+            realScoreTwo += realFreeThrow;
+        }
+        $(".scoreTwo").text(realScoreTwo);
+    }
+}
+
+function updateTeams(){
+    $(".teamOne").text(realTeamOneName);
+    $(".teamTwo").text(realTeamTwoName);
+}
+
